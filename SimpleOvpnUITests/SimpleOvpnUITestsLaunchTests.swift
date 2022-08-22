0d22b4ebe9542faa244135202b55e114760a8f7b
Long Kim Hoang <longkimhoang@outlook.com>
2022-08-22 20:30:02 +0700

    Integrate swift-format

--- SimpleOvpnUITests/SimpleOvpnUITestsLaunchTests.swift
+++ SimpleOvpnUITests/SimpleOvpnUITestsLaunchTests.swift
@@ -9,13 +9,9 @@ import XCTest
 
 class SimpleOvpnUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
+    override class var runsForEachTargetApplicationUIConfiguration: Bool { true }
 
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
+    override func setUpWithError() throws { continueAfterFailure = false }
 
     func testLaunch() throws {
         let app = XCUIApplication()
